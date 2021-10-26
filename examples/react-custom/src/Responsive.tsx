8818664c5577a07dec579195329b0e0c08351013
Karibash <y.karibayashi@gmail.com>
2021-10-27 02:17:22 +0900

    Add a use case to the example project

--- examples/react-custom/src/Responsive.tsx
+++ examples/react-custom/src/Responsive.tsx
@@ -5,11 +5,15 @@ const Responsive: React.FC = () => {
   const isLessThanSmall = useMediQ('max-sm');
   const isGreaterThanMedium = useMediQ('min-md');
   const isBetweenSmallAndMedium = useMediQ('min-sm-and-max-md');
+  const isLessThanSmallOrGreaterThanLarge1 = useMediQ('max-sm-or-min-lg');
+  const isLessThanSmallOrGreaterThanLarge2 = useMediQ(['max-sm', 'min-lg']);
   return (
     <>
       {isLessThanSmall && <div>isLessThanSmall</div>}
       {isGreaterThanMedium && <div>isGreaterThanMedium</div>}
       {isBetweenSmallAndMedium && <div>isBetweenSmallAndMedium</div>}
+      {isLessThanSmallOrGreaterThanLarge1 && <div>isLessThanSmallOrGreaterThanLarge1</div>}
+      {isLessThanSmallOrGreaterThanLarge2 && <div>isLessThanSmallOrGreaterThanLarge2</div>}
     </>
   );
 };
--- examples/styled-custom/src/Responsive.tsx
+++ examples/styled-custom/src/Responsive.tsx
@@ -14,11 +14,15 @@ const Responsive: React.FC = () => {
   const isLessThanSmall = useMediQ('max-sm');
   const isGreaterThanMedium = useMediQ('min-md');
   const isBetweenSmallAndMedium = useMediQ('min-sm-and-max-md');
+  const isLessThanSmallOrGreaterThanLarge1 = useMediQ('max-sm-or-min-lg');
+  const isLessThanSmallOrGreaterThanLarge2 = useMediQ(['max-sm', 'min-lg']);
   return (
     <Wrapper>
       {isLessThanSmall && <div>isLessThanSmall</div>}
       {isGreaterThanMedium && <div>isGreaterThanMedium</div>}
       {isBetweenSmallAndMedium && <div>isBetweenSmallAndMedium</div>}
+      {isLessThanSmallOrGreaterThanLarge1 && <div>isLessThanSmallOrGreaterThanLarge1</div>}
+      {isLessThanSmallOrGreaterThanLarge2 && <div>isLessThanSmallOrGreaterThanLarge2</div>}
     </Wrapper>
   );
 };
