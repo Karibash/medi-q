911b40280ba1632a43c645cfe7b6df17b8862d2d
Karibash <y.karibayashi@gmail.com>
2021-11-26 01:51:44 +0900

    Add a test for the normal case

---
+++ packages/styled/test/index.test.tsx
@@ -0,0 +1,129 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import styled from 'styled-components';
+import { styleSheetSerializer } from 'jest-styled-components';
+
+import { createMediQ } from '@medi-q/core';
+import { ThemeProvider } from '@medi-q/styled';
+
+expect.addSnapshotSerializer(styleSheetSerializer);
+
+document.documentElement.style.fontSize = '16px';
+const mediQ = createMediQ({
+  tiny: '400px',
+  small: '600px',
+  medium: '800px',
+  large: '1000px',
+});
+
+describe('@medi-q/styled', () => {
+  it('min query', () => {
+    const Component = styled.div`
+      color: #000000;
+
+      ${props => props.theme.mediQ('min-tiny')} {
+        background: #000001;
+      }
+
+      ${props => props.theme.mediQ('min-small')} {
+        background: #000002;
+      }
+
+      ${props => props.theme.mediQ('min-medium')} {
+        background: #000003;
+      }
+
+      ${props => props.theme.mediQ('min-large')} {
+        background: #000004;
+      }
+    `;
+
+    const { container } = render(
+      <ThemeProvider theme={{}} mediQ={mediQ}>
+        <Component />
+      </ThemeProvider>,
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('max query', () => {
+    const Component = styled.div`
+      color: #000000;
+
+      ${props => props.theme.mediQ('max-tiny')} {
+        background: #000001;
+      }
+
+      ${props => props.theme.mediQ('max-small')} {
+        background: #000002;
+      }
+
+      ${props => props.theme.mediQ('max-medium')} {
+        background: #000003;
+      }
+
+      ${props => props.theme.mediQ('max-large')} {
+        background: #000004;
+      }
+    `;
+
+    const { container } = render(
+      <ThemeProvider theme={{}} mediQ={mediQ}>
+        <Component />
+      </ThemeProvider>,
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('composite query', () => {
+    const Component = styled.div`
+      color: #000000;
+
+      ${props => props.theme.mediQ('min-tiny-and-max-tiny')} {
+        background: #000001;
+      }
+
+      ${props => props.theme.mediQ('min-small-and-max-small')} {
+        background: #000002;
+      }
+
+      ${props => props.theme.mediQ('min-medium-and-max-medium')} {
+        background: #000003;
+      }
+
+      ${props => props.theme.mediQ('min-large-and-max-large')} {
+        background: #000004;
+      }
+
+      ${props => props.theme.mediQ('max-tiny-or-min-tiny')} {
+        background: #000005;
+      }
+
+      ${props => props.theme.mediQ('max-small-or-min-small')} {
+        background: #000006;
+      }
+
+      ${props => props.theme.mediQ('max-medium-or-min-medium')} {
+        background: #000007;
+      }
+
+      ${props => props.theme.mediQ('max-large-or-min-large')} {
+        background: #000008;
+      }
+    `;
+
+    const { container } = render(
+      <ThemeProvider theme={{}} mediQ={mediQ}>
+        <Component />
+      </ThemeProvider>,
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
